f47997a70522830cee4acbd5792ca93c95ab151e
danilaSADev <danila25up@gmail.com>
2021-12-16 17:08:13 +0200

    #22: Added classes for Demo Balance
    - added DemoExchange and DemoSpot

---
+++ CryptoTerminal/Models/DemoExchanges/DemoExchange.cs
@@ -0,0 +1,17 @@
+﻿using CryptoTerminal.Models.CryptoExchanges;
+
+namespace CryptoTerminal.Models
+{
+    public class DemoExchange : CryptoExchanges.CryptoExchange
+    {
+        public override CryptoSpot GetCryptoSpot()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<CryptoFutures> GetFutures()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CryptoTerminal/Models/DemoExchanges/DemoSpot.cs
@@ -0,0 +1,45 @@
+﻿using CryptoTerminal.Models.CryptoExchanges;
+using Binance.Net;
+
+namespace CryptoTerminal.Models
+{
+    public class DemoSpot : CryptoExchanges.CryptoSpot
+    {
+        
+
+        public override void CancelOrder(SpotOrder order)
+        {
+            
+        }
+
+        public override List<CoinBalance> GetCoinBalances()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<SpotOrder> GetDepthOfMarket()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<SpotOrder> GetOpenOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<SpotOrder> GetOrderHistory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Transaction> GetTransactionsHistory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MakeOrderResult MakeOrder(SpotOrder order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
