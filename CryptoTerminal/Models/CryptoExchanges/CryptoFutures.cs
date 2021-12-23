c816803e5214889affa196b2b68b26d11c68219a
danilaSADev <danilas25up@gmail.com>
2021-12-23 11:58:49 +0200

    #218 : Added basic structure for Binance Futures USDT

--- CryptoTerminal/Models/CryptoExchanges/CryptoFutures.cs
+++ CryptoTerminal/Models/CryptoExchanges/CryptoFutures.cs
@@ -1,4 +1,6 @@
-﻿namespace CryptoTerminal.Models.CryptoExchanges
+﻿using CryptoExchange.Net.Attributes;
+
+namespace CryptoTerminal.Models.CryptoExchanges
 {
     public abstract class CryptoFutures
     {
@@ -10,5 +12,25 @@
         }
 
         public string MainCoin => _mainCoin;
+
+        public abstract Task<FuturesOrder> GetOrdersHistory();
+
+        public abstract Task<FuturesOrder> GetOpenOrders();
+
+        public abstract Task<OrderBook> GetDepthOfMarket();
+
+        public abstract Task<CoinBalance> GetUSDTBalance();
+
+        public abstract void AdjustLeverage(int value);
+
+        public abstract void ChangeMarginType();
+
+        public Task<CoinBalance> GetUSDTFreeBalance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract Task<MakeOrderResult> MakeOrder(FuturesOrder order);
+
     }
 }
