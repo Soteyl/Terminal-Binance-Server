a59eaa24590ea030d932a91bd42d5b9fa851a79d
danilaSADev <danilas25up@gmail.com>
2021-12-24 00:12:32 +0200

    #210 : Simplified method structure

--- CryptoTerminal/Models/CryptoExchanges/BinanceRealisation/BinanceFuturesUSDT.cs
+++ CryptoTerminal/Models/CryptoExchanges/BinanceRealisation/BinanceFuturesUSDT.cs
@@ -1,5 +1,6 @@
 ﻿using Binance.Net.Interfaces;
 using Binance.Net.Interfaces.SubClients.Futures;
+using Binance.Net.Objects.Futures.FuturesData;
 using Binance.Net.Objects;
 using CryptoExchange.Net.ExchangeInterfaces;
 using CryptoExchange.Net.Interfaces;
@@ -45,11 +46,9 @@ namespace CryptoTerminal.Models.CryptoExchanges.BinanceRealisation
 
         public override async Task<CoinBalance> GetUSDTBalance()
         {
-            var task = await _client.Account.GetBalanceAsync();
+            WebCallResult<IEnumerable<BinanceFuturesAccountBalance>> balanceResult = await _client.Account.GetBalanceAsync();
 
-            bool isSuccess = task.GetResultOrError(out var balanceResult, out Error? error);
-
-            var coinBalanceUSDT = balanceResult?.ToList().Find(balance => balance.Asset == MainCoin);
+            var coinBalanceUSDT = balanceResult.Data.ToList().Find(balance => balance.Asset == MainCoin);
 
             return new CoinBalance(MainCoin, coinBalanceUSDT?.AvailableBalance ?? 0, (coinBalanceUSDT?.WalletBalance - coinBalanceUSDT?.AvailableBalance) ?? 0);
         }
