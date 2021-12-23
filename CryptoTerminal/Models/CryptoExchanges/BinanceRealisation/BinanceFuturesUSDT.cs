25e16861cc3404a669a886eadd0e914150607b42
danilaSADev <danilas25up@gmail.com>
2021-12-23 23:51:35 +0200

    #210: Implemented GetUSDTBalance

--- CryptoTerminal/Models/CryptoExchanges/BinanceRealisation/BinanceFuturesUSDT.cs
+++ CryptoTerminal/Models/CryptoExchanges/BinanceRealisation/BinanceFuturesUSDT.cs
@@ -8,11 +8,11 @@ using CryptoExchange.Net.Objects;
 
 namespace CryptoTerminal.Models.CryptoExchanges.BinanceRealisation
 {
-    public class BinanceFutures : CryptoFutures
+    public class BinanceFuturesUSDT : CryptoFutures
     {
         private IBinanceClientFuturesUsdt _client;
         
-        public BinanceFutures(IBinanceClient binanceClient, string mainCoin) 
+        public BinanceFuturesUSDT(IBinanceClient binanceClient, string mainCoin) 
             : base(mainCoin)
         {
             _client = binanceClient.FuturesUsdt;
@@ -43,9 +43,15 @@ namespace CryptoTerminal.Models.CryptoExchanges.BinanceRealisation
             throw new NotImplementedException();
         }
 
-        public override Task<CoinBalance> GetUSDTBalance()
+        public override async Task<CoinBalance> GetUSDTBalance()
         {
-            throw new NotImplementedException();
+            var task = await _client.Account.GetBalanceAsync();
+
+            bool isSuccess = task.GetResultOrError(out var balanceResult, out Error? error);
+
+            var coinBalanceUSDT = balanceResult?.ToList().Find(balance => balance.Asset == MainCoin);
+
+            return new CoinBalance(MainCoin, coinBalanceUSDT?.AvailableBalance ?? 0, (coinBalanceUSDT?.WalletBalance - coinBalanceUSDT?.AvailableBalance) ?? 0);
         }
 
         public override async Task<MakeOrderResult> MakeOrder(FuturesOrder order)
