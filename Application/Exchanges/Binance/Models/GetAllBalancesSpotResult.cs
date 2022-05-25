c12fbf5dd75969c8026ef796952eeeca683dafa6
Daniil Korovin <61512362+danilaSADev@users.noreply.github.com>
2022-05-25 09:13:54 +0300

    #267 : added comment for GetAllBalancesSpotResult

--- Application/Exchanges/Binance/Models/GetAllBalancesSpotResult.cs
+++ Application/Exchanges/Binance/Models/GetAllBalancesSpotResult.cs
@@ -2,6 +2,9 @@
 
 namespace Ixcent.CryptoTerminal.Application.Exchanges.Binance.Models
 {
+    /// <summary>
+    /// Result for getting all Binance balances.
+    /// </summary>
     public class GetAllBalancesSpotResult
     {
         public IEnumerable<BinanceBalance> AvailableBalances { get; set; }
