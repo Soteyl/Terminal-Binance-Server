7eac9e90ecbb8f3e611b05984e40a2211c1fd205
Daniil Korovin <61512362+danilaSADev@users.noreply.github.com>
2022-06-06 20:19:25 +0300

    #269 : fixed namespace order in the class

--- Application/Exchanges/Binance/Spot/Models/OrdersHistoryModel.cs
+++ Application/Exchanges/Binance/Spot/Models/OrdersHistoryModel.cs
@@ -1,8 +1,8 @@
 ﻿using MediatR;
+using Ixcent.CryptoTerminal.Application.Exchanges.Binance.Spot.Results;
 
 namespace Ixcent.CryptoTerminal.Application.Exchanges.Binance.Spot.Models
 {
-    using Results;
     public class OrdersHistoryModel : IRequest<OrdersHistoryResult>
     {
         public string Symbol { get; set; } = string.Empty;
